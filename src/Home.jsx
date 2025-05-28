6d11e266bb83728138f572ee4bf5a5bf5c2a263c
DoNguyenTai <donguyentai05@gmail.com>
2025-05-28 10:43:13 +0700

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -30,15 +30,15 @@ const Home = () => {
     });
   }, []);
   return (
-    <div className="w-full lgl:w-[85%] h-full lgl:h-[85%] bg-transparent text-white z-50 flex items-start justify-between p-4 lgl:p-0">
+    <div className="w-full lgl:w-[85%] h-full lgl:h-[85%] bg-transparent text-white z-50 flex md:flex-row flex-col items-start justify-between p-4 lgl:p-0">
 
-      <div className="w-16 h-96 bg-transparent hidden lgl:flex flex-col gap-4">
+      <div className="md:w-16 md:h-96 w-full bg-transparent flex-row flex md:flex-col gap-4 pb-2">
 
         <div
           onClick={() => setSidenav(true)}
-          className="w-full h-20 bg-bodyColor rounded-3xl flex justify-center items-center cursor-pointer group"
+          className="md:w-full w-[80px] h-20  bg-bodyColor rounded-3xl flex justify-center items-center cursor-pointer group"
         >
-          <div className="flex flex-col gap-1.5 overflow-hidden">
+          <div className="flex flex-col gap-1.5 p-4 overflow-hidden">
             <span className="w-8 h-[2px] bg-textColor inline-block -translate-x-2 group-hover:translate-x-0 transition-transform duration-300 group-hover:bg-designColor"></span>
             <span className="w-8 h-[2px] bg-textColor inline-block group-hover:bg-designColor duration-300"></span>
             <span className="w-8 h-[2px] bg-textColor inline-block -translate-x-3.5 group-hover:translate-x-0 transition-transform duration-300 group-hover:bg-designColor"></span>
@@ -70,7 +70,7 @@ const Home = () => {
         )}
 
 
-        <div className="w-full h-80 bg-bodyColor rounded-3xl flex flex-col items-center justify-between py-6">
+        <div className="w-full md:h-80 h-[80px] bg-bodyColor rounded-3xl flex md:flex-col flex-row items-center justify-between py-6">
 
           <span
             onClick={() =>
@@ -86,28 +86,29 @@ const Home = () => {
           // className="w-full h-6 text-textColor text-xl flex items-center justify-center hover:text-designColor duration-300 cursor-pointer relative group"
           >
             <FaUser />
-            <span className="text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl absolute left-0 translate-x-8 group-hover:translate-x-12 opacity-0 group-hover:opacity-100 transition-all duration-300 z-20">
+            <span className="text-black font-medium md:block hidden text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl absolute left-0 translate-x-8 group-hover:translate-x-12 opacity-0 group-hover:opacity-100 transition-all duration-300 z-20">
               About
             </span>
           </span>
           {/* Resume Icon */}
-          <span
-            onClick={() =>
-              setAbout(false) &
-              setResume(true) &
-              setProjects(false) &
-              setContact(false)
-            }
+          <div
+            onClick={() => {
+              setAbout(false);
+              setResume(true);
+              setProjects(false);
+              setContact(false);
+
+            }}
             className={`${resume
                 ? "text-designColor"
                 : "w-full h-6 text-textColor text-xl flex items-center justify-center hover:text-designColor duration-300 cursor-pointer relative group"
               } w-full h-6 text-xl flex items-center justify-center hover:text-designColor duration-300 cursor-pointer relative group`}
           >
             <IoIosPaper />
-            <span className="text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl absolute left-0 translate-x-8 group-hover:translate-x-12 opacity-0 group-hover:opacity-100 transition-all duration-300 z-20">
+            <span className="text-black font-medium hidden md:block text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl absolute left-0 translate-x-8 group-hover:translate-x-12 opacity-0 group-hover:opacity-100 transition-all duration-300 z-20">
               Resume
             </span>
-          </span>
+          </div>
 
           <span
             onClick={() =>
@@ -122,7 +123,7 @@ const Home = () => {
               } w-full h-6 text-xl flex items-center justify-center hover:text-designColor duration-300 cursor-pointer relative group`}
           >
             <MdWork />
-            <span className="text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl absolute left-0 translate-x-8 group-hover:translate-x-12 opacity-0 group-hover:opacity-100 transition-all duration-300 z-20">
+            <span className="text-black font-medium hidden md:block text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl absolute left-0 translate-x-8 group-hover:translate-x-12 opacity-0 group-hover:opacity-100 transition-all duration-300 z-20">
               Projects
             </span>
           </span>
@@ -141,7 +142,7 @@ const Home = () => {
               } w-full h-6 text-xl flex items-center justify-center hover:text-designColor duration-300 cursor-pointer relative group`}
           >
             <FaEnvelope />
-            <span className="text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl absolute left-0 translate-x-8 group-hover:translate-x-12 opacity-0 group-hover:opacity-100 transition-all duration-300 z-20">
+            <span className="text-black font-medium hidden md:block text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl absolute left-0 translate-x-8 group-hover:translate-x-12 opacity-0 group-hover:opacity-100 transition-all duration-300 z-20">
               Contact
             </span>
           </span>
@@ -156,10 +157,12 @@ const Home = () => {
 
         <div className="w-full lgl:w-8/12 h-[95%] bg-bodyColor rounded-2xl flex justify-center items-center">
 
-          <div className="w-full h-full lgl:hidden bg-transparent rounded-2xl flex flex-col gap-6">
-            <About />
-            
-          </div>
+          <div className="w-full h-full lgl:hidden bg-transparent rounded-2xl flex flex-col gap-6 overflow-y-scroll scrollbar-thin scrollbar-thumb-[#646464]">
+  {about && <About />}
+  {resume && <Resume />}
+  {projects && <Projects />}
+  {contact && <Contact />}
+</div>
 
           <div  ref={scrollRef}
  className="w-full h-[96%] hidden lg:block justify-center overflow-y-scroll scrollbar-thin scrollbar-thumb-[#646464]">
